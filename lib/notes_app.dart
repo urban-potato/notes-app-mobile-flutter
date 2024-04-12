469a238b24792ea26d50185cdc24445e04af8ef9
Urban Potato <veryprogramming@gmail.com>
2024-04-12 18:39:53 +0700

    Preparations

---
+++ lib/notes_app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:notes_app/features/notes_list/index.dart';
+import 'package:notes_app/generated/l10n.dart';
+
+class NotesApp extends StatefulWidget {
+  const NotesApp({super.key});
+
+  @override
+  State<NotesApp> createState() => _NotesAppState();
+}
+
+class _NotesAppState extends State<NotesApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: const NotesListScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
